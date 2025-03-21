eda48254ccd685c7fea26e78da966e3d0389d273
JALLI CHAKRAVARTHI <jallichakravarthi@JALLIs-Mac-mini.local>
2025-03-22 01:01:22 +0530

    Added missing EditWatchListItem file

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://mern-watchlist-zew7.vercel.app", credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
